36a19f862ac6a3d246c2923a80c3817c549fa112
Robert <vannestr@uwindsor.ca>
2024-02-08 18:07:56 -0500

    initial commit

---
+++ app/src/main/java/com/example/listviewapplication/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.listviewapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+
+//for snackbard
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    EditText editText;
+    Button btn_add;
+   // String list[] = {"item1", "item2", "item3", "item4","item5"};
+
+    ArrayList<String> arrayList;
+
+
+    ArrayAdapter arrayAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        editText =findViewById(R.id.editTextText);
+        btn_add = findViewById(R.id.button);
+
+        //arrayList.add("Data1");
+        //arrayList.add("Data2");
+        //arrayList.add("Data3");
+        arrayList = FileHandler.readData(MainActivity.this);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+
+
+//        // Set an OnClickListener to the button 'btn_add'. This means that the code inside onClick will be executed when the button is clicked.
+//        btn_add.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // Retrieve the text entered in the editText field and convert it to a String.
+//                String itemText = editText.getText().toString();
+//
+//                // Check if the retrieved text is not empty.
+//                if (!itemText.isEmpty()) {
+//                    // If the text is not empty, add it to the 'arrayList'.
+//                    arrayList.add(itemText);
+//                    // Call the 'writeData' method of 'FileHandler' to save the updated 'arrayList' into a file.
+//                    FileHandler.writeData(arrayList, getApplicationContext());
+//                    // Notify the 'arrayAdapter' that the data set has changed so it can update the ListView.
+//                    arrayAdapter.notifyDataSetChanged();
+//                    // Clear the editText field to make it ready for the next item input.
+//                    editText.setText("");
+//                } else {
+//                    // If the text is empty, show a Snackbar message on the screen informing the user that an empty item cannot be added.
+//                    // The Snackbar is displayed at the bottom of the screen and lasts for a short duration (LENGTH_LONG).
+//                    Snackbar.make(view, "Cannot add empty item!", Snackbar.LENGTH_LONG).show();
+//                }
+//            }
+//        });
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String itemText = editText.getText().toString();
+                if (!itemText.isEmpty()) {
+                    arrayList.add(itemText);
+                    FileHandler.writeData(arrayList, getApplicationContext());
+                    arrayAdapter.notifyDataSetChanged();
+                    editText.setText("");
+                } else {
+                    Snackbar.make(view, "Cannot add empty item!", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                arrayList.remove(i);
+                arrayAdapter.notifyDataSetChanged();
+                FileHandler.writeData(arrayList, getApplicationContext());
+            }
+        });
+
+
+    }
+}
